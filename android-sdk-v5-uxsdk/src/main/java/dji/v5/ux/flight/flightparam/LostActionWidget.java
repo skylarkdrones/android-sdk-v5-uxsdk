878560d6a478fed898dc00c8005aad944b0f5f09
Bishyan Kar <bishyan@skylarkdrones.com>
2025-02-05 20:10:34 +0530

    refactor: Ensure no other app can change the failsafe action

--- android-sdk-v5-uxsdk/src/main/java/dji/v5/ux/flight/flightparam/LostActionWidget.java
+++ android-sdk-v5-uxsdk/src/main/java/dji/v5/ux/flight/flightparam/LostActionWidget.java
@@ -41,10 +41,12 @@ public class LostActionWidget extends ConstraintLayoutWidget<Object> {
     protected void initView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         inflate(context, R.layout.uxsdk_widget_flight_lost_action, this);
         spinnerCell = findViewById(R.id.setting_menu_aircraft_failSafe);
-        spinnerCell.addOnItemSelectedListener(position -> {
-            FailsafeAction value = FailsafeAction.find(position);
-            widgetModel.setLostAction(value).subscribe();
-        });
+        if (!isCertificationBuild) {
+            spinnerCell.addOnItemSelectedListener(position -> {
+                FailsafeAction value = FailsafeAction.find(position);
+                widgetModel.setLostAction(value).subscribe();
+            });
+        }
     }
 
     private void setupLostSignalAction() {
@@ -60,6 +62,9 @@ public class LostActionWidget extends ConstraintLayoutWidget<Object> {
     }
 
     private void updateSelection(FailsafeAction failsafeAction) {
+        if (isCertificationBuild && failsafeAction != FailsafeAction.GOHOME) {
+            widgetModel.setLostAction(FailsafeAction.GOHOME).subscribe();
+        }
         spinnerCell.select(failsafeAction.value());
     }
 
