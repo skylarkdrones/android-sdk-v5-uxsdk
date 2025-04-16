de6ba23a928da95683190b6694e030a0d592f8c9
Bishyan Kar <bishyan@skylarkdrones.com>
2025-04-16 12:48:21 +0530

    feat: Hide HomePointWidget

--- android-sdk-v5-uxsdk/src/main/java/dji/v5/ux/core/ui/setting/fragment/FlycMenuFragment.java
+++ android-sdk-v5-uxsdk/src/main/java/dji/v5/ux/core/ui/setting/fragment/FlycMenuFragment.java
@@ -11,6 +11,7 @@ import dji.v5.utils.common.StringUtils;
 import dji.v5.ux.R;
 import dji.v5.ux.core.base.TextCell;
 import dji.v5.ux.core.ui.setting.ui.MenuFragment;
+import dji.v5.ux.core.widget.CertificationUtils;
 
 /**
  * Description :
@@ -40,5 +41,13 @@ public class FlycMenuFragment extends MenuFragment {
             addFragment(getFragmentManager(), fragment, true);
         });
 
+        hideHomePointWidget(view);
+    }
+
+    private void hideHomePointWidget(View view) {
+        if (CertificationUtils.INSTANCE.isCertificationBuild()) {
+            view.findViewById(R.id.homePointWidget).setVisibility(View.GONE);
+            view.findViewById(R.id.dividerHomePointWidget).setVisibility(View.GONE);
+        }
     }
 }
